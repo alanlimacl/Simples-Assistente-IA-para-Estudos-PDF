56ab5b65c0fb619de6e78050648871d5055572af
Alan Lima <alancoite123@hotmail.com>
2026-01-30 19:52:12 -0300

    Inicialização

---
+++ visual.py
@@ -0,0 +1,26 @@
+tittle_html = """
+<style>
+@keyframes fadeInUp {
+  0% {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+.title-container {
+  text-align: center;
+  animation: fadeInUp 1s ease-out;
+}
+h1 {
+  margin-bottom: -8px;
+}
+</style>
+
+<div class='title-container'>
+  <h1>Bem-vindo ao Oráculo</h1>
+  <p style='color: gray; font-size: 18px;'>O seu Assistente Virtual</p>
+</div>
+"""
